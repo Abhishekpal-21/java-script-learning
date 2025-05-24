ff0faca24d043fb4518b9958d6cb2beda20bce40
Abhishek Pal <abhishekpal21102004@gmail.com>
2025-05-24 16:23:16 +0000

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict";  /*treat code as new version of js*/
+// alert(3+3)  we are using nodejs not browser
+
+
+
+// number => 2 to power 53
+// bigint
+// string = " "
+// boolean = true/false
+//  null = standalone value
+//  undefined = 
+//  symbol = unique
+
+
+
+
+// Conversion and operations
+let score = "33"
+let ValueinNumber = Number(score)
+console.log (typeof(ValueinNumber))
